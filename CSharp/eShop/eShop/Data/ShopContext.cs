6481b507365147c2ab399dc66ece8f132ad4f8a7
Didier Baertschiger <dbaertschiger@jadeworld.com>
2020-12-18 16:59:37 +0100

    Initial commit

---
+++ CSharp/eShop/eShop/Data/ShopContext.cs
@@ -0,0 +1,26 @@
+﻿using eShop.Models;
+using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eShop.Data
+{
+    public class ShopContext : IDisposable
+    {
+        private LiteDatabase _db;
+
+        public ILiteCollection<Product> Products => _db.GetCollection<Product>("products");
+
+        public ShopContext()
+        {
+            _db = new LiteDatabase(@"eshop.db");
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+    }
+}
---
+++ CSharp/eShop/eShop/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace eShop.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ProductState State { get; set; }
+    }
+}
