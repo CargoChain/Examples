aea1a554bad9f62cdda64423db5af2d1f70811a3
Didier Baertschiger <dbaertschiger@jadeworld.com>
2020-12-29 11:06:03 +0100

    Remove HTTPs configuration for the carrier application

--- CSharp/eShop/eShopCarrier/Startup.cs
+++ CSharp/eShop/eShopCarrier/Startup.cs
@@ -1,13 +1,9 @@
+using eShop.Lib;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace eShop.Carrier
 {
@@ -23,6 +19,8 @@ namespace eShop.Carrier
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CargoChainConfiguration>(Configuration.GetSection("CargoChain"));
+
             services.AddControllersWithViews();
         }
 
@@ -36,10 +34,7 @@ namespace eShop.Carrier
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
